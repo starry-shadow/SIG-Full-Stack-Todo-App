7cf32cbf07d339a28e1ddeb55bef8914921605db
Shikhar Verma <shikharv2@protonmail.com>
2025-02-26 16:26:35 -0500

    Meeting 1 Commit

---
+++ src/components/Todo/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+function Todo(){
+    return(
+        <>
+            <h1 class="title">Todo List</h1>
+            <div class="container">
+                <div class="search-bar">
+                    <input type="search" class="search-input"></input>
+                </div>
+                <h2>Todo Tasks</h2>
+                <button class="addButton">+</button>
+            </div>
+        </>
+    );
+}
+export default Todo
